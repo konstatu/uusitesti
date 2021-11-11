96ea3eae253d852442daf3a41aa36d072ca6a8db
Konsta Turunen <konsta.turunen@student.laurea.fi>
2021-11-11 16:49:53 +0200

    serverimuutoksia

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
+const PORT = process.env.PORT || 8080;
+// Luodaan web-palvelin ja kerrotaan siitä tieto konsolissa.
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT );
+});
 
 // server listening 
 server.listen(port, () => {
